47239284ad4b36635a94b51766410ab349ab3cbb
bondieffects <36088638+bondieffects@users.noreply.github.com>
2025-03-25 22:15:28 +1100

    added some better comments to the buzzer module

--- src/buzzer.c
+++ src/buzzer.c
@@ -3,7 +3,11 @@
 
 static float volumes[10] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
 
-
+/*! @brief Initialise the buzzer object
+    @param buzzer The buzzer object
+    @param pin The pin the buzzer is connected to
+    @param port The port the buzzer is connected to
+*/
 void create_buzzer(buzzer_t *buzzer, uint8_t pin, volatile uint8_t *port)
 {
   buzzer->port = port;
@@ -11,6 +15,12 @@ void create_buzzer(buzzer_t *buzzer, uint8_t pin, volatile uint8_t *port)
   buzzer->duty_cycle = volumes[4]; // default 50% volume
 }
 
+/*! @brief Plays a note on a buzzer a set frequency, amplitude and for a set interval
+    @param buzzer The buzzer object
+    @param freq The frequency of the note
+    @param volume The volume of the note
+    @param interval The interval to play the note for
+*/
 void buzzer_play(buzzer_t* buzzer, float freq, uint8_t volume, double interval)
 {
     float duty_cycle = volumes[volume];
