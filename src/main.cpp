772500df69e95812f9cbdc6221445e6f2e0330d0
DeevoDitchi <danielbhe6@gmail.com>
2025-03-25 22:00:01 +1100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,58 +13,47 @@
 // Pinout
 #define PIN_BUZZER_P (PD2) 
 #define PORT_BUZZER_P (&PORTD)
-#define BUTTON_CASE1 (PD3)
-#define BUTTON_CASE2 (PD4)
+#define BUTTON (PD3)
+
 
 // Global Variables
-button_t button_case1, button_case2;
+button_t button;
 buzzer_t buzzer;
 alarm_t alarm;
 int adc = 0;
 
 // Interrupt Service Routines
-ISR(PCINT2_vect)
+ISR(INT1_vect)
 {
   _delay_ms(DEBOUNCE_INTERVAL); // debounce
-  button_case1.state = PIND >> BUTTON_CASE1 & 1;  // read the pin state
-
-  if (button_case1.state) {                        // updates on rising edge
-        alarm.volume = (alarm.volume + 1) % 2;
-        usart_send_string(">case number:");
-        usart_send_num(alarm.volume, 0, 0);
-        usart_send_string("\n");
+  button.state = PIND >> BUTTON & 1;        // read the pin state
+  if (button.state) {                          // updates on rising edge
+    alarm.enabled = !alarm.enabled;       // Toggle the alarm enable flag
+    usart_send_string(">case number:");
+    usart_send_num(alarm.enabled, 0, 0);
+    usart_send_string("\n");
   }
 }
-
 ISR(ADC_vect){
   adc = ADC;
 }
-
 int main(void)
 {
    /////////////////////////// PIN CONFIGURATION ///////////////////////////
     usart_init(9600);
     DDRD |= (1 << PIN_BUZZER_P);
-    DDRD &= ~((1 << BUTTON_CASE1) | (1 << BUTTON_CASE2));
-    PORTD |= ((1 << BUTTON_CASE1) | (1 << BUTTON_CASE2)); // set the bit to enable pullup
-
+    DDRD &= ~((1 << BUTTON) | (1 << BUTTON));
     /////////////////////////// INTERRUPT CONFIGURATION ///////////////////////////
-
     // Configure External Interrupt for CASE 1 button (INT1)
     EIMSK |= (1 << INT1);                 // enable INT1
     EICRA |= (1 << ISC11) | (1 << ISC10); // rising edge
-
-    // Configure Pin Change Interrupt for CASE 2 button (PCINT20)
-    PCICR |= (1 << PCIE2);          // enables the interrupts on PORTD
-    PCMSK2 |= (1 << PCINT20);       // enables PCINT20
     /////////////////////////// CREATE OBJECTS ///////////////////////////
-      // Create the buzzer, alarm and button objects
-      create_button(&button_case1);
-      create_button(&button_case2);
-      usart_send_string("buttons created\n");
-    
-    while (1)
-    {
+    // Create the buzzer, alarm and button objects
+    create_buzzer(&buzzer, PIN_BUZZER_P, PORT_BUZZER_P);
+    usart_send_string("buzzer created\n");
+    create_button(&button);
+    usart_send_string("button created\n");
+    while (1){
         bitSet(ADMUX, REFS0);
         bitSet(ADMUX, MUX1); // 1 
         bitSet(ADMUX, MUX0); // 1 
@@ -72,7 +61,6 @@ int main(void)
         ADCSRA |= 0b111 < 0;
         bitSet(ADCSRA, ADIE);
         bitSet(ADCSRA, ADEN);
-      
         sei();
       
         bitSet(ADCSRA, ADSC);
