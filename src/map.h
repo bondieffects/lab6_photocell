693e2f437d2ed50ea8d9ae8296da1d3f5b36a600
DeevoDitchi <danielbhe6@gmail.com>
2025-03-26 23:49:45 +1100

    Update map.h

--- src/map.h
+++ src/map.h
@@ -1,17 +1,28 @@
 
 
-    #include <avr/io.h>
-    #include <util/delay.h>
-    #include <stdlib.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
 
 
-    int32_t map_(int32_t x, int32_t x1, int32_t x2, int32_t y1, int32_t y2){
-        // Using int64_t for intermediate calculation to prevent overflow
-        int64_t scaled_value = (x - x1) * (y2 - y1);
-    //(y2 - y1) total difference in height from starting point y1 to highest point y2
-    //(x-x1) how far x has moved from the starting point
-    //multiplying these two gets us the scaled value for dividing by (x2-x1)
-        return (scaled_value / (x2 - x1) + y1);
-    //divided by (x2-x1) in order to get a proportional change
-    // then +y1 to get final position of 
-    }
+int32_t map_increase(float x, float x1, float x2, float y1, float y2){
+    float k = (y2-y1)/(x2-x1); //since y2>y1 therefore increasing
+    // since y increases with x we use the formula below
+    return y1+k*(x-x1);
+    // maps the x value to the corresponding y value
+    //(x-x1) gives the distance from value from starting point (x1)
+    // slope (k) then used to scale the distance between x and x1
+    // y1+k as it shifts the linear mapping to start at the correct pt due to previous (y2-y1) which gives the total height 
+    // increased from y1 therefore y1+k
+}
+
+int32_t map_decrease(float x, float x1, float x2, float y1, float y2){
+    float k = (y1-y2)/(x2-x1);//since y1>y2 therefore decreasing
+    // since y decreases with x we use the formula below
+    return y1-k*(x-x1);
+    // maps the x value to the corresponding y value
+    //(x-x1) gives the distance from value from starting point (x1)
+    // slope (k) then used to scale the distance between x and x1 
+    // y1-k as it shifts the linear mapping to start at the correct pt due to previous (y1-y2) which gives the total height
+    // started at y1 and decreased therefore y1-k
+} 
