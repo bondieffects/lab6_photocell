d6443ae58164a9a71e451878203243d654662111
DeevoDitchi <danielbhe6@gmail.com>
2025-03-26 23:23:48 +1100

    Update map.h

--- src/map.h
+++ src/map.h
@@ -1,17 +1,17 @@
 
 
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdlib.h>
+    #include <avr/io.h>
+    #include <util/delay.h>
+    #include <stdlib.h>
 
 
-int32_t map_int(int32_t x, int32_t x1, int32_t x2, int32_t y1, int32_t y2) {
-    // Using int64_t for intermediate calculation to prevent overflow
-    int64_t scaled_value = (x - x1) * (y2 - y1);
-//(y2 - y1) height difference between 2 points 
-//(x-x1) how far x has moved from the starting point
-//multiplying these two 
-    return (scaled_value / (x2 - x1) + y1);
-//
-    
-}
+    int32_t map_(int32_t x, int32_t x1, int32_t x2, int32_t y1, int32_t y2){
+        // Using int64_t for intermediate calculation to prevent overflow
+        int64_t scaled_value = (x - x1) * (y2 - y1);
+    //(y2 - y1) total difference in height from starting point y1 to highest point y2
+    //(x-x1) how far x has moved from the starting point
+    //multiplying these two gets us the scaled value for dividing by (x2-x1)
+        return (scaled_value / (x2 - x1) + y1);
+    //divided by (x2-x1) in order to get a proportional change
+    // then +y1 to get final position of 
+    }
