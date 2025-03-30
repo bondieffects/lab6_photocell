49ad24034a896cb1ca8ccd24eb19aae4c0f23ee7
bondieffects <36088638+bondieffects@users.noreply.github.com>
2025-03-30 15:22:19 +1100

    Add photocell calibration functions and modify ADC initialization

--- src/adc.c
+++ src/adc.c
@@ -1,10 +1,11 @@
 #include "adc.h"
 #include <avr/io.h>
 #include "util/delay.h"
+#include "usart.h"
 
 /*! @brief Configures the ADC 
 */
-void adc_init(void)
+void adc_init(adc_t *adc)
 {
     ADMUX |= (  _BV(REFS0) |    // Reference AVCC
                 _BV(MUX1) |     // Select ADC channel 3
@@ -13,6 +14,8 @@ void adc_init(void)
                 _BV(ADIE) |     // Enable interrupts
                 _BV(ADEN));     // Enable the ADC
     // ADCSRB = 0 for free-running mode
+
+    adc->value = 0;         // Initialise to 0
 }
 
 /*! @brief Triggers the start conversion
@@ -22,3 +25,38 @@ void adc_start_conversion(void)
     ADCSRA |= _BV(ADSC); // trigger the start conversion 
     _delay_ms(10);
 }
+
+/*! @brief Initialise the photocell values */
+void photocell_init(photocell_t *photocell)
+{
+    photocell->photocell_min = 1023; // Set high so can calibrate down
+    photocell->photocell_max = 0;    // Set low so can calibrate up
+}
+
+/*! @brief Automatically calibrate the photocell min and max values
+    @param photocell: the photocell object
+    @note Should be called after the ADC is initialised and the ADC interrupt is enabled.
+*/
+bool photocell_calibrate(photocell_t *photocell, adc_t *adc, bool input)
+{
+    adc_start_conversion();
+    _delay_ms(10); // Wait for the ADC to settle
+
+    if (adc->value < photocell->photocell_min) {
+        photocell->photocell_min = adc->value; // Set the minimum value
+        usart_send_string("photocell_min: ");
+        usart_send_num(photocell->photocell_min, 3, 0);
+        usart_send_string("\n\r");
+    }
+    if (adc->value > photocell->photocell_max) {
+        photocell->photocell_max = adc->value; // Set the maximum value
+        usart_send_string("photocell_max: ");
+        usart_send_num(photocell->photocell_max, 4, 0);
+        usart_send_string("\n\r");
+    }
+    if (input) return 1;    // escape when the button is pressed
+    else return 0;
+}
+
+
+
--- src/adc.h
+++ src/adc.h
@@ -1,22 +1,32 @@
 #ifndef ADC_H
 #define ADC_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 #ifdef __cplusplus
 
     extern "C" {
 
 #endif
 
-#define PHOTOCELL_MAX (519)
-#define PHOTOCELL_MIN (287)
+#define PHOTOCELL_MAX (890)
+#define PHOTOCELL_MIN (449)
 
 typedef struct {
-
+    uint16_t value;      // Current ADC value
 } adc_t;
 
-void adc_init(void);
+typedef struct {
+    uint16_t photocell_min;        // Minimum ADC value
+    uint16_t photocell_max;        // Maximum ADC value
+} photocell_t;
+
+void adc_init(adc_t *adc);
 void adc_start_conversion(void);
 
+void photocell_init(photocell_t *photocell);
+bool photocell_calibrate(photocell_t *photocell, adc_t *adc, bool input);
 
 #ifdef __cplusplus
 
